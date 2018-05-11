06a46f880ecf029c82482c8e7b45975b73a94c9f
Unknown <h.chuk@auckland.ac.nz>
2018-05-12 10:43:38 +1200

    Finished clean up of project

--- ImageProcessor/app/src/main/java/com/bankai/bleach/imageprocessor/ProcessingFunctions.java
+++ ImageProcessor/app/src/main/java/com/bankai/bleach/imageprocessor/ProcessingFunctions.java
@@ -1,15 +1,7 @@
 package com.bankai.bleach.imageprocessor;
-import org.opencv.core.Mat;
-import org.opencv.imgproc.Imgproc;
-import org.opencv.imgcodecs.Imgcodecs;
-import org.opencv.core.Size;
-import org.opencv.core.Core;
-import java.util.ArrayList;
 import java.io.File;
 import android.os.Environment;
-import android.util.Log;
 
-import static android.util.Log.ASSERT;
 
 public class ProcessingFunctions {
 
